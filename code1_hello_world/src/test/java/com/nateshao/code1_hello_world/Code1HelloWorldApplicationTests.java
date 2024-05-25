3375b6610602976ba2fb8653a3eb8f283b0b71f0
nateshao <shaotongjie@gmail.com>
2024-05-25 22:22:51 +0800

    hello

---
+++ code1_hello_world/src/test/java/com/nateshao/code1_hello_world/Code1HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.code1_hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Code1HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
