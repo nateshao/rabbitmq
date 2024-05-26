03f67eac4730d5ae65d634bfbf781911f8078ee8
nateshao <shaotongjie@gmail.com>
2024-05-26 13:45:34 +0800

    路由模式

---
+++ code4_routing/src/test/java/com/nateshao/routing/Code4RoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.routing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Code4RoutingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
