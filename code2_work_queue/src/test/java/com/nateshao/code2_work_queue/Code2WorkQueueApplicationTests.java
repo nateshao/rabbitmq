4fdec0ddaa58e38b5d407a0931dc99cf3b77f15d
nateshao <shaotongjie@gmail.com>
2024-05-26 13:23:07 +0800

    发布订阅模式

---
+++ code2_work_queue/src/test/java/com/nateshao/code2_work_queue/Code2WorkQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.code2_work_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Code2WorkQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ code3_publish_subscribe/src/test/java/com/nateshao/pubsub/Code3PubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.pubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Code3PubsubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
