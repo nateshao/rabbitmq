3cf69818b8e9614a3a89faf0427b5d38549a8ef0
nateshao <shaotongjie@gmail.com>
2024-05-27 00:12:27 +0800

    producer

--- code6_springboot_producer/src/main/java/com/nateshao/code6_springboot_producer/mq/RabbitMQProducerMainType.java
+++ code6_springboot_producer/src/main/java/com/nateshao/code6_springboot_producer/mq/RabbitMQProducerMainType.java
@@ -1,13 +1,13 @@
-package com.atguigu.mq;  
-  
-import org.springframework.boot.SpringApplication;  
-import org.springframework.boot.autoconfigure.SpringBootApplication;  
-  
+package com.nateshao.code6_springboot_producer.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class RabbitMQProducerMainType {
 
     public static void main(String[] args) {
-        SpringApplication.run(RabbitMQProducerMainType.class, args);  
+        SpringApplication.run(RabbitMQProducerMainType.class, args);
     }
 
 }
