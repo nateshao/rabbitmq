ba604feb8dd7bae0ce5a7252fbd1b737f441094b
nateshao <shaotongjie@gmail.com>
2024-05-30 00:14:25 +0800

    rabbit mq 路由模式

--- code4_routing/src/main/java/com/nateshao/routing/Consumer1.java
+++ code4_routing/src/main/java/com/nateshao/routing/Consumer1.java
@@ -2,39 +2,28 @@ package com.nateshao.routing;
 
 import com.nateshao.routing.util.ConnectionUtil;
 import com.rabbitmq.client.*;
-import java.io.IOException;  
-  /**
+
+import java.io.IOException;
+
+/**
  * @Author 千羽
  * @公众号 程序员千羽
  * @Date 2024/5/29 16:00
  * @Version 1.0
  */
-public class Consumer1 {  
-  
-    public static void main(String[] args) throws Exception {  
-  
+public class Consumer1 {
+    public static void main(String[] args) throws Exception {
         Connection connection = ConnectionUtil.getConnection();
-  
-        Channel channel = connection.createChannel();  
-  
-        String queue1Name = "test_direct_queue1";  
-  
-        channel.queueDeclare(queue1Name,true,false,false,null);  
-  
-        Consumer consumer = new DefaultConsumer(channel){  
-  
-            @Override  
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {  
-  
-                System.out.println("body："+new String(body));  
-                System.out.println("Consumer1 将日志信息打印到控制台.....");  
-  
-            }  
-  
-        };  
-  
-        channel.basicConsume(queue1Name,true,consumer);  
-  
-    }  
-  
+        Channel channel = connection.createChannel();
+        String queue1Name = "test_direct_queue1";
+        channel.queueDeclare(queue1Name, true, false, false, null);
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("body：" + new String(body));
+                System.out.println("Consumer1 将日志信息打印到控制台.....");
+            }
+        };
+        channel.basicConsume(queue1Name, true, consumer);
+    }
 }
